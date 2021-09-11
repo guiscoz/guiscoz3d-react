970071028c22c8278ee9c5e9bbebf1309d8057bf
guiscoz <71405193+guiscoz@users.noreply.github.com>
2021-09-10 21:28:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-//para colocar no Github: https://cli.github.com/     video 2 da aula 14
 
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
