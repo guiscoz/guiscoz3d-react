4dbae72c5addca603791e8e55f10c23b20f1c3d7
guiscoz <71405193+guiscoz@users.noreply.github.com>
2021-09-10 21:28:42 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+//para colocar no Github: https://cli.github.com/     video 2 da aula 14
 
-function App() {
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Models from "./components/Models";
+import Animations from "./components/Animations";
+import Rigging from "./components/Rigging";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/models">
+          <Models />
+        </Route>
+        <Route path="/animations">
+          <Animations />
+        </Route>
+        <Route path="/rigs">
+          <Rigging />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
